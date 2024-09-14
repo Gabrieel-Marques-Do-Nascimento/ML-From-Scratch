3af5b3063a0dcf4bf877b92a327f9e413e7031a7
Gabriel marques do nascimento <gabrielmarquesnascimentob@gmail.com>
2024-09-14 20:47:00 -0300

    upp

--- machine_learning_from_Scratch/knn.py
+++ machine_learning_from_Scratch/knn.py
@@ -1,18 +1,45 @@
 """
+CONHECIDO COMO APRENDIZADO PREGUICOSO
+-----------------------------------------------------
+Exemplos reais de aplicação do KNN:
+Reconhecimento de padrões de imagem:
+
+O KNN pode ser usado para classificar imagens, por exemplo, na detecção de dígitos escritos à mão (como o famoso
+dataset MNIST). O algoritmo classifica cada imagem de um dígito com base nos vizinhos mais próximos no espaço de
+características da imagem.
+
+Classificação de textos (Spam/Não Spam):
+
+O KNN pode ser aplicado para classificar e-mails em "spam" ou "não spam" com base em suas características (como a
+frequência de palavras, presença de certos termos, etc.).
+
+Previsão de doenças médicas:
+
+No campo médico, o KNN é usado para prever o diagnóstico de doenças com base em atributos do paciente (idade,
+níveis de colesterol, etc.) comparando com casos já conhecidos.
+
+Sistemas de recomendação:
+
+Em sistemas de recomendação, como sugerir produtos similares com base nas compras de outros usuários. O KNN pode
+comparar a similaridade de usuários ou produtos, recomendando aqueles que estão mais "próximos" no espaço de
+características.
+
 /storage/emulated/0/__Projetos__/ML-From-Scratch/machine_learning_from_Scratch/KNN.py
 """
-import numpy as np
 from collections import Counter
+
+import numpy as np
+
 
 def eucliendean_distance(x1, x2):
     """
-    
     """
+
     # raisquadrada(
     #        somar( (x1 - x2 ) elevado a 2)
     #  cada componente do vetor
     return np.sqrt(np.sum((x1 - x2) ** 2))
-    
+
 
 class KNN:
     def __init__(self, k=3):
@@ -20,41 +47,35 @@ class KNN:
         k: o numero de visinhos mais proximos.defaut( 3 )
         """
         self.k = k
-        
+
     def fit(self, X, y):
-       """
-       metodo de ajuste, isso ajustara as amostras de treinamento e alguns rotulos de treinamento, envolvera uma etapa de trinamento
-       """    
-       self.X_train = X
-       self.y_train = y
+        """
+        metodo de ajuste, isso ajustara as amostras de treinamento e alguns rotulos de treinamento, envolvera uma etapa de trinamento
+        """
+        self.X_train = X
+        self.y_train = y
 
     def predict(self, X):
-       """
-    metodo de previsao, para prever novas amostras
-       """
-       predicted_labels = [self._predict(x) for x in X]
-       return np.array(predicted_labels)
-       
+        """
+     metodo de previsao, para prever novas amostras
+        """
+        predicted_labels = [self._predict(x) for x in X]
+        return np.array(predicted_labels)
+
     def _predict(self, x):
         # caucular as distancias
         # compute distances
         distances = [eucliendean_distance(x, x_train) for x_train in self.X_train]
-        print("distances=",distances)
+        # print("distances=", distances)
+
         # get k nearest samples, labels
         # obitemos a amostra mais procima, e obiter os label
         # matriz das distancias mais procimas
         k_indices = np.argsort(distances)[:self.k]
         # rotulos mais procimos
         k_nearest_labels = [self.y_train[i] for i in k_indices]
-        
-        
+
         # fazemos uma votacao de maturidade, para obiter o rotulo de class label mais comum
         most_common = Counter(k_nearest_labels).most_common(1)
         # metodo= def most_common()
         return most_common[0][0]
-        
-        
-    
-    
-
- 
--- machine_learning_from_Scratch/knntest.py
+++ machine_learning_from_Scratch/knntest.py
@@ -1,18 +1,18 @@
 """
 /storage/emulated/0/__Projetos__/ML-From-Scratch/machine_learning_from_Scratch/knntest.py
 """
-import numpy as np
+from matplotlib.colors import ListedColormap
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-cmap = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
+import numpy as np
+
+cmap = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
 iris = datasets.load_iris()
 X, y = iris.data, iris.target
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state =1234)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
 """
 print(X_train.shape)
 print(X_train[0])
@@ -45,7 +45,7 @@ predictions = clf.predict(X_test)
 # caucular a precisao, ver quais previsoes estao corretas
 # soma cada previsao que for igual y_test
 # divide pelo numero de amostras do y_test
-acc = np.sum(predictions ==y_test) / len(y_test)
+acc = np.sum(predictions == y_test) / len(y_test)
 # teste 1 == 1.0 estao corretos,   k=3 agora altera para k=5, normslmente sao numeros impars
 # teste 2 == 966666666666 , nao foi tao bom mais , esta funcionando corretamente
 print(acc)
